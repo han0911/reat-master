535b8930a47584a2f9848ff1303a26ddfeee1b67
han0911 <a01099249559@gmail.com>
2025-09-16 17:34:22 +0900

    ㅎ

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
+import styled from "styled-components";
+import "./App.css";
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.textcolor};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
-import './App.css'
+const Button = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+`;
 
-function App() {
-  return (
-    <>
-    </>
-  )
+type AppProps = {
+  D: boolean;
+  setD: React.Dispatch<React.SetStateAction<boolean>>;
+};
+declare module "styled-components" {
+  export interface DefaultTheme {
+    background: string;
+    textcolor: string;
+  }
 }
 
-export default App
+function App({ D, setD }: AppProps) {
+  return (
+    <Container>
+      <h1>{D ? "다크 모드" : "라이트 모드"}</h1>
+      <Button onClick={() => setD((prev) => !prev)}>
+        {D ? "라이트 모드로" : "다크 모드로"}
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,25 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+const darkTheme = {
+  background: "black",
+  textcolor: "white",
+};
+const lightTheme = {
+  background: "white",
+  textcolor: "black",
+};
+
+function Main() {
+  const [D, setD] = useState(false);
+  return (
+    <ThemeProvider theme={D ? darkTheme : lightTheme}>
+      <App D={D} setD={setD} />
+    </ThemeProvider>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(<Main />);
